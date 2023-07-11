6579c27cc107ec7edec2922a9c0089dacc850861
zion <130053047+tsitiii@users.noreply.github.com>
2023-07-11 03:16:58 +0300

    Update and rename FizzBuzz.py to Grading of students.py

--- Grading of students.py
+++ Grading of students.py
@@ -1,17 +1,9 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
 
-#
-# Complete the 'gradingStudents' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts INTEGER_ARRAY grades as parameter.
-#
 
 def gradingStudents(grades):
     # Write your code here
